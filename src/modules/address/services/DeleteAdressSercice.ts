b2b3d2501b892e8764462e93fa41286803be3309
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-14 23:19:28 -0300

    AddressService add: Update and Delete

---
+++ src/modules/address/services/DeleteAdressSercice.ts
@@ -0,0 +1,21 @@
+import AppError from 'src/shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AddressRepository from '../typeorm/repository/AddressRepository';
+
+interface IAddress {
+  id: string;
+}
+
+class DeleteAdressSercice {
+  public async execute({ id }: IAddress): Promise<void> {
+    const addressRepository = getCustomRepository(AddressRepository);
+
+    const address = await addressRepository.findOne(id);
+    if (!address) {
+      throw new AppError('Endereço não encontrado', 404);
+    }
+    await addressRepository.remove(address);
+  }
+}
+
+export default new DeleteAdressSercice();
---
+++ src/modules/address/services/UpdataAddressService.ts
@@ -0,0 +1,55 @@
+import AppError from 'src/shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Address from '../typeorm/entities/Address';
+import AddressRepository from '../typeorm/repository/AddressRepository';
+import DeleteAdressSercice from '../typeorm/repository/AddressRepository';
+
+interface IAddress {
+  id: string;
+  cep: string;
+  cidade: string;
+  estado: string;
+  bairro: string;
+  rua: string;
+  numero: string;
+  complemento: string;
+  tipo: string;
+  endereco_primario: boolean;
+}
+
+class UpdataAddressService {
+  public async execute({
+    id,
+    cep,
+    bairro,
+    cidade,
+    complemento,
+    endereco_primario,
+    estado,
+    numero,
+    rua,
+    tipo,
+  }: IAddress): Promise<Address> {
+    const addressRepository = getCustomRepository(AddressRepository);
+
+    const address = await addressRepository.findOne(id);
+    if (!address) {
+      throw new AppError('Endereço não encontrado', 404);
+    }
+    address.bairro = bairro;
+    address.cep = cep;
+    address.cidade = cidade;
+    address.complemento = complemento;
+    address.endereco_primario = endereco_primario;
+    address.estado = estado;
+    address.numero = numero;
+    address.rua = rua;
+    address.tipo = tipo;
+
+    await addressRepository.save(address);
+
+    return address;
+  }
+}
+
+export default new UpdataAddressService();
