369dade7affbfe523ae92041a95d9baad2abe71c
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-15 01:29:14 -0300

    Address Controller add methods update and delete

---
+++ src/modules/address/controller/AddressController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import DeleteAdressSercice from '../services/DeleteAdressSercice';
+import UpdataAddressService from '../services/UpdataAddressService';
+
+class AddressController {
+  public async update(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const {
+      bairro,
+      cep,
+      cidade,
+      complemento,
+      endereco_primario,
+      estado,
+      numero,
+      rua,
+      tipo,
+    } = req.body;
+    const address = await UpdataAddressService.execute({
+      id,
+      bairro,
+      cep,
+      cidade,
+      complemento,
+      endereco_primario,
+      estado,
+      numero,
+      rua,
+      tipo,
+    });
+    return res.status(200).json(address);
+  }
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    await DeleteAdressSercice.execute({ id });
+    return res.status(204).json([]);
+  }
+}
+
+export default new AddressController();
